6ae30b6cd7892ca3bd8fa44d2181213c45d4720a
Wallace H Wu <wallace_h_wu@wistron.com>
2020-07-17 22:37:48 +0800

    update

--- diff.js
+++ diff.js
@@ -1,3 +1,8 @@
+export const REPLACE = 0; // 替換掉原來的 node, 例如把原本的 div 換成了 ul
+export const REORDER = 1; // 移動、刪除、新增子節點, 例如原本有一個 ul, 把它跟其他同層的 node 順序互換
+export const PROPS = 2; // 修改了節點的 props
+export const TEXT = 3; // 文字類型的 node 修改, 文字內容可能會改變, 例如本來的一個 'test' 改成 'test2'
+
 /* === Diff 算法 === */
 
 export function diff(oldTree, newTree) {
@@ -9,6 +14,13 @@ export function diff(oldTree, newTree) {
 }
 
 function completeDiffCheck(oldNode, newNode, index, patches) {
+
+    console.log('wwwwwwwwwwwwwwwwwwwwwwwwww');
+    console.log(oldNode);
+    console.log(newNode);
+    console.log(index);
+    console.log(patches);
+
     let currentPatch = []
 
     // node 的移除
@@ -22,7 +34,7 @@ function completeDiffCheck(oldNode, newNode, index, patches) {
         // 如果新舊節點不同, 表示真實 dom 也需刷新, 紀錄進去 Patch
         if (newNode !== oldNode) {
             // type 是 TEXT 類型
-            currentPatch.push({type: patchType.TEXT, content: newNode})
+            currentPatch.push({type: TEXT, content: newNode})
         }
         return;
     }
@@ -31,21 +43,24 @@ function completeDiffCheck(oldNode, newNode, index, patches) {
     if (oldNode.tagName === newNode.tagName && oldNode.key === newNode.key) {
         // 檢查 props
         let propsPatches = propsDiffCheck(oldNode, newNode);
+        console.log('//////////////////////////');
+        console.log(propsPatches);
 
         // 如果檢查 props 後, 發現新舊兩個有差異
         if (propsPatches) {
             // props 有差異, 表示真實 dom 也需刷新, 紀錄進去 Patch
             // type 是 PROPS
-            currentPatch.push({type: patchType.PROPS, props: propsPatches});
+            currentPatch.push({type: PROPS, props: propsPatches});
         }
 
         // 檢查 children. 如果新節點的 children 不是 ignore 的話, 就做 children diff check
         if (!isIgnoreChildren(newNode)) {
+            console.log(';;;;;;;;;;;;;;;;;;;;;;;;;;;;;');
             childrenDiffCheck(oldNode.children, newNode.children, index, patches, currentPatch);
         }
     } else {
         // 新舊節點不相同時, 直接判定是差異, 紀錄進去 Patch
-        currentPatch.push({type: patchType.REPLACE, node: newNode})
+        currentPatch.push({type: REPLACE, node: newNode})
     }
 
     // 若是 currentPatch 有長度, 表示有差異, 並把記錄到的全部差異指向當前 index 的位置
@@ -102,25 +117,32 @@ function isIgnoreChildren (node) {
 
 // 對新舊兩棵 tree 的子節點 做檢查 (深度優先)
 function childrenDiffCheck(oldChildren, newChildren, index, patches) {
+    console.log('------------------------------------');
+    console.log(oldChildren);
+    console.log(newChildren);
+    console.log(index);
+    console.log(patches);
     // left Node 為深度優先, 是從左邊開始往下檢查
     let leftNode = null;
 
     // 最初始為 0, 因為從 tree 最上層開始
     let currentNodeIndex = index;
 
-    // 從舊 tree 的 Children 下手
-    oldChildren.forEach((child, index) => {
-        // 從 newChildren 中取出對應要比較的 New Node
-        let newChildNode = newChildren[index];
+    // 若 oldChildren 不為空, 從舊 tree 的 Children 下手
+    if (!oldChildren) {
+        oldChildren.forEach((child, index) => {
+            // 從 newChildren 中取出對應要比較的 New Node
+            let newChildNode = newChildren[index];
 
-        // 計算 Node 的標誌
-        currentNodeIndex = (leftNode && leftNode.count)
-            // 若 index & count 屬性為空 => 0 + null + 1 = 1
-            ? currentNodeIndex + leftNode.count + 1
+            // 計算 Node 的標誌
+            currentNodeIndex = (leftNode && leftNode.count)
+                // 若 index & count 屬性為空 => 0 + null + 1 = 1
+                ? currentNodeIndex + leftNode.count + 1
 
-            // 若 index 為 1 => 1 + 1 = 2 以此類推
-            : currentNodeIndex + 1
-        completeDiffCheck(child, newChildNode, currentNodeIndex, patches) // 遞迴, 且深度優先的歷遍子節點
-        leftNode = child;
-    })
+                // 若 index 為 1 => 1 + 1 = 2 以此類推
+                : currentNodeIndex + 1
+            completeDiffCheck(child, newChildNode, currentNodeIndex, patches) // 遞迴, 且深度優先的歷遍子節點
+            leftNode = child;
+        })
+    }
 }
